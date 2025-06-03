dd2d9799e9744c219c156094f62c1eb494b1362b
r <r@g.com>
2025-06-03 10:07:18 +0530

    payment

--- playwright.config.js
+++ playwright.config.js
@@ -28,7 +28,9 @@ module.exports = defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
+    screenshot: 'on',
+    video:'on'
   },
 
   /* Configure projects for major browsers */
